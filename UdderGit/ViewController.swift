f27834f630b85fbc82c360bd1bcfaa24c629829c
Robson Kuca <rtkuca@hotmail.com>
2019-07-05 23:43:06 -0300

    Adicionado ViewDidappear

--- UdderGit/ViewController.swift
+++ UdderGit/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // so testando
+    }
 
 
 }
